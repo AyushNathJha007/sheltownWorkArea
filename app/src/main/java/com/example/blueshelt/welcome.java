56b97e6545c2994838c685370a2e6518d4c1fda7
Adarsh vatsa <adarsh.vatsa007@gmail.com>
2020-11-30 10:05:44 +0530

    second commit

--- app/src/main/java/com/example/blueshelt/welcome.java
+++ app/src/main/java/com/example/blueshelt/welcome.java
@@ -2,7 +2,9 @@ package com.example.blueshelt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class welcome extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void goto_signup(View view) {
+
+        startActivity(new Intent(getApplicationContext(),signup.class));
     }
 }
